19c4ddff399f546e07d0f8c432f485417cea441d
Russ WIlliams <Russ_Williams@live.com>
2015-03-07 10:06:16 -0500

    starting submit button functions
    ajax will need specific work for chrome

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//globale variables
+//global variables
 	var output = ["Description: "]; //an  empty array, this will be the end result
 	document.getElementById('output-area').innerHTML = output;
 	
@@ -20,3 +20,18 @@
 		output.splice(1, output.length); //use splice to remove all units after the first ("Description: ").
 		document.getElementById('output-area').innerHTML = output;
 	}//end resetOutputFunction
+	
+//Submit function for ajaxing the content from the txt files.
+	function createDescriptionFunction() {
+		//create request object.
+		var  ajaxSubject= new XMLHttpRequest();
+		
+		//send request.
+		ajaxSubject.open("GET", "subjects.js", true); //method, url of source, async?
+		ajaxSubject.send();
+		
+		//get the response.
+		var answer = ajaxSubject.responseText;
+		document.getElementById('output-area').innerHTML = answer;
+	}//end createDescriptionFunction.
+	
---
+++ subjects.js
@@ -0,0 +1,50 @@
+{
+  "name": "My extension",
+  ...
+  "permissions": [
+    "http://www.google.com/"
+  ],
+  ...
+}
+//Create arrays to pull from for drawing descriptions.
+
+//Drawing Materials
+var drawingMaterials = [
+	"pen",
+	"pencil",
+	"paper"
+	"sketch book",
+	"pencil sharpener",
+	"easel"
+];
+
+//Office Supplies
+var officeSupplies = [
+	"desk",
+	"task chair",
+	"manager's chair'",
+	"executive chair",
+	"desktop computer",
+	"laptop",
+	"monitor",
+	"keyboard",
+	"mouse",
+	"mouse pad",
+	"printer",
+	"file cabinet",
+	"hanging files",
+	"note pad",
+	"cubicle"
+	"name tag"
+	"in box",
+	"out box",
+	"reception desk",
+	"paper clip"
+	"stapler",
+	"lamp",
+	"letter opener",
+	"file folder",
+	"supply cabinet",
+	"mail machine",
+	"business card"
+];
