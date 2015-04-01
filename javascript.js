30ba7d74496d11428774e6e39e3da58b239aedea
Russ WIlliams <Russ_Williams@live.com>
2015-04-01 19:49:15 -0400

    improvements on button functionality
    Nearly completed functions for the buttons.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@
 
 //add-subject function - onclick function to display "subject" in the output-area
 	function addSubjectFunction() {
-		output.push("SUBJECT ");
+		output.push("SUBJECT");
 		document.getElementById('output-area').innerHTML = output.join(" ");
 	} //end addSubjectFunction
 	
@@ -22,27 +22,62 @@
 	}//end resetOutputFunction
 	
 //Submit function for ajaxing the content from the xml files.
+	
+	/*notes for selecting random subject in xml file.
+		1.) Find total number of entries in subject xml file.
+			1.a.) may need to exclude the wrapper.
+		2.) Store total # as a variable (example: subjectDBTotalEntryCount).
+		3.) feed variable to the following equation:
+			Math.floor(Math.random() * subjectDBTotalEntryCount) + 1);
+		4.) Store this in a variable (example: chosenSubjectPosition).
+		5.) locate array position that matches chosenSubjectPosition.
+		6.) pull unit from array and return to output array in the appropriate position.*/
+	
 	function createDescriptionFunction() {
 		//create request object.
-		var  ajaxSubject= new XMLHttpRequest();
-		
+		var  ajaxSubject= new XMLHttpRequest(); //variable for subjects xml array
+					
 		//get the response.
 		ajaxSubject.onreadystatechange=function() {
 			if (ajaxSubject.readyState==4 && ajaxSubject.status==200) {
-				//assign response to a variable.
-				var answer =  ajaxSubject.responseXML;
-				//get parts from document.
-				var x = answer.getElementsByTagName("drawingMaterial");
-				//sent to output area.
-				for (i = 0; i < x.length; i++) {
-					var text = text + x[i].innerHTML;
-				}//end for statement
-				document.getElementById('output-area').innerHTML = text;
-			}//end if statement
-		}//end state change function
+				var answer =  ajaxSubject.responseXML; //assign response to a variable.
+				
+				var subjects = answer.getElementsByTagName("name"); //get parts from document.
+				var ajaxSubjectTotal= subjects.length - 1; //total # of entries in the subject xml array
+				
+				//choose a random number between 0 and total # of entries in subjects xml array
+				var max = ajaxSubjectTotal; //high end of number range.
+				var min = 0; //lowest possible number in range.
+				var randomNumber = Math.floor((Math.random() * (max - min + 1)) + min);  
+
+				//send to output area.
+				var chosenSubjectPosition = subjects[randomNumber].innerHTML; //get value of randomly selected subject
+				
+				
+				//write a check to see what the position in the array says and act according
+				for (i = 0; i < output.length; i++) {
+					
+					if(output[i] == "SUBJECT") { //if the position says "SUBJECT"
+						output[i] = chosenSubjectPosition;	//put subject into output array
+						
+						//find a way to change each output position independently at the same time.
+					}
+					
+				} //end for loop, check for output position contents
+					
+				
+				document.getElementById('output-area').innerHTML = output.join(" ");//re-display new complete output array.
+				
+			} //end if statement
+		} //end state change function
+			
+			//send request.
+			ajaxSubject.open("POST", "subjects.xml", true); //method, url of source, async?
+			ajaxSubject.send();
 		
-		//send request.
-		ajaxSubject.open("POST", "subjects.xml", true); //method, url of source, async?
-		ajaxSubject.send();
-	}//end createDescriptionFunction.
+	}//end createDescriptionFunction()
+	
+	
+	
+	
 	
